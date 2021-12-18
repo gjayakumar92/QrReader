4bb1339c3adf5c2ef760575d2280199bec9cd253
gjayakumar92 <gjayakumar92@gmail.com>
2021-12-18 08:57:00 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-wj9dhr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-wj9dhr)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+import QrCodeReader from './QrCodeReader';
+
+export default function App() {
+  return (
+    <div>
+      <h1>QR Code Reader</h1>
+      <QrCodeReader />
+    </div>
+  );
+}
